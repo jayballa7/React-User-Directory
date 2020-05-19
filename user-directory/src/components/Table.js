c7863e628e627d9904fd4eed04cdbaa537fe9fd3
Jennifer Ballard <jenn_ballard7@hotmail.com>
2020-05-18 17:12:25 -0700

    table can be sorted by name

--- user-directory/src/components/Table.js
+++ user-directory/src/components/Table.js
@@ -2,22 +2,6 @@ import React, { Component } from "react";
 import API from "../utils/employees-api";
 import "../styles/Table.css";
 
-
-const sortTypes = {
-	up: {
-		class: 'sort-up',
-		fn: (a, b) => a.name - b.name
-	},
-	down: {
-		class: 'sort-down',
-		fn: (a, b) => b.name - a.name
-	},
-	default: {
-		class: 'sort',
-		fn: (a, b) => a
-	}
-};
-
 export default class Table extends Component {
    constructor(props) {
       super(props)
@@ -26,22 +10,44 @@ export default class Table extends Component {
                {
                  login: '', name: '', email: '', phone: '', location: ''
                }
-            ]
+            ],
+            results: []
          }
    }
+    firstResults = [];
+
     headers = [
        {name: 'Name'},
        {name: 'Email'},
        {name: 'Phone #'},
        {name: 'Location'}
-   ]
+   ];
 
    componentDidMount() {
       API.getEmployees().then(res => {
-         this.setState({ employees: res.data.results })
+         this.setState({ employees: res.data.results });
+         this.firstResults = res.data.results
       })
       .catch(err => console.log(err));
    }
+
+   sortName = (event) => {
+      this.setState({
+        sort: !this.state.sort
+      }, () => {
+  
+        if(this.state.sort){
+          this.setState({
+            employees: this.state.employees.slice().sort((a,b)=> (a.name.last > b.name.last ? 1 : -1))
+          })
+        }
+        else{
+          this.setState({
+            employees: this.state.employees.slice().sort((a,b)=> (b.name.last > a.name.last ? 1 : -1))
+          })
+        }
+      })
+  }
 
    renderTableData() {
       return this.state.employees.map((employees) => {
@@ -60,14 +66,14 @@ export default class Table extends Component {
    }
   
    render() {
-      const { data } = this.props;
+      // const { data } = this.props;
       return (
          <div>
             <table className = "table">
                <thead>
                   <tr>
                      {this.headers.map(({name}) => {
-                        return(<th key = {name}>{name}</th>)
+                        return(<th key = {name} onClick={this.sortName}>{name}</th>)
                      })}
                   </tr>
                </thead>
