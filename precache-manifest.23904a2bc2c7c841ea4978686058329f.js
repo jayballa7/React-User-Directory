712c4bb5cc215d1a1a904e76e3909d39d9afac45
Jennifer Ballard <jenn_ballard7@hotmail.com>
2020-05-29 14:33:03 -0700

    Updates

---
+++ precache-manifest.23904a2bc2c7c841ea4978686058329f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5bdf3eaf1224e7c02785b2eae839a48",
+    "url": "/React-User-Directory/index.html"
+  },
+  {
+    "revision": "c9c7874bd8294494b130",
+    "url": "/React-User-Directory/static/css/main.40e7aca7.chunk.css"
+  },
+  {
+    "revision": "ab9cbb7dfea9e07092ec",
+    "url": "/React-User-Directory/static/js/2.e556f114.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-User-Directory/static/js/2.e556f114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c7874bd8294494b130",
+    "url": "/React-User-Directory/static/js/main.3430a95e.chunk.js"
+  },
+  {
+    "revision": "cc6e1130269edc2f6abe",
+    "url": "/React-User-Directory/static/js/runtime-main.4da53361.js"
+  }
+]);
